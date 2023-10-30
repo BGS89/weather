f95f6ff2574dc7f1dbc6dea8da244a759f9175dd
Ben Sivyer <bensivyer@Bens-MacBook-Pro-2.local>
2023-10-30 19:27:45 +0000

    fetch three day forecast data from api

---
+++ src/data/weatherData.js
@@ -0,0 +1,17 @@
+import axios from "axios";
+
+const key = import.meta.env.VITE_SOME_KEY;
+
+const weatherApi = axios.create({
+  baseURL: "http://api.weatherapi.com/v1",
+});
+
+const fetchForecast = (city) => {
+  return weatherApi
+    .get(`/forecast.json?key=${key}&q=${city}&days=10&aqi=no&alerts=no`)
+    .then((response) => {
+      return response.data.forecast.forecastday;
+    });
+};
+
+export { fetchForecast };
